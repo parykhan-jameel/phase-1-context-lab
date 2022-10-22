fe40b7efe07040958e04a9170c1debaf7f155798
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-22 18:20:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Your Code Here */
 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +21,60 @@ const allWagesFor = function () {
 
     return payable
 }
+const createEmployeeRecord = function (recArray){
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    };
+};
 
+const createEmployeeRecords = function (recsArray){
+    return recsArray.map(rec=>createEmployeeRecord(rec));
+};
+
+const createTimeInEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const createTimeOutEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const hoursWorkedOnDate = function (date){
+    const inEvent = this.timeInEvents.find(inEvent=>inEvent.date === date);
+    const outEvent = this.timeOutEvents.find(outEvent=>outEvent.date === date);
+            return ((outEvent.hour - inEvent.hour)/100)
+};
+
+const wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.apply(this, [date])* this.payPerHour
+};
+
+const findEmployeeByFirstName = function (srcArray, targetName){
+    for(let i=0; i<srcArray.length;i++){
+        if(srcArray[i].firstName===targetName){
+            return srcArray[i]
+        }}
+};
+
+const calculatePayroll = function(recsArray){
+    let payroll = []
+    recsArray.forEach(element => payroll.push(allWagesFor.call(element)))
+    return payroll.reduce((a,b)=>{return a+b})
+}
